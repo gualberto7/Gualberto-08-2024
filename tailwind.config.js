2131519ed43f6421c58f4fba09bd3e762979e1ad
gcuiza <gualbertocuizav@gmail.com>
2024-08-16 08:00:47 -0400

    Added palette colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#2196f3',
+        secondary: '#b88bf5',
+        success: '#4caf50',
+        warning: '#fb8c00',
+        danger: '#c51162'
+      }
+    }
   },
   plugins: []
 }
